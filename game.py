d1c64c26e903637368a60b8dfc45afe689083cea
alexandre pollin <alexandre.pollin@socgen.com>
2020-01-21 15:49:22 +0100

    Game development with TDD

---
+++ game.py
@@ -0,0 +1,19 @@
+import random
+import string
+
+class Game:
+    def __init__(self):
+        self.grid = []
+
+        for _ in range(9):
+            self.grid.append(random.choice(string.ascii_uppercase))
+
+
+    def is_valid(self, word):
+        if word == '':
+            return False
+
+        for letter in word:
+            if letter not in self.grid:
+                return False
+        return True
